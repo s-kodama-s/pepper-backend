b9b8334b5167432bbc25f5712e7f8f1904a05755
斉藤大明 <saito-hiroaki@saitoudaiminnoMacBook-Air.local>
2024-02-19 20:22:54 +0900

    [feat]テストコード削除

--- src/tests/api/jsonplaceholder.test.ts
+++
@@ -1,9 +0,0 @@
-// import { fetchUsers } from '../../api/jsonplaceholder';
-
-// describe('jsonplaceholder', () => {
-//   it('should return users', async () => {
-//     const users = await fetchUsers();
-//     console.log(users);
-//     expect(users).toBeDefined();
-//   });
-// });
--- src/tests/api/openAi.test.ts
+++
@@ -1,9 +0,0 @@
-// import { openAi } from '../../api/summarizeText';
-
-// describe('openAi', () => {
-//   it('sample test', async () => {
-//     const result = await openAi();
-//     console.log(result);
-//     expect(1).toBe(1);
-//   });
-// });
