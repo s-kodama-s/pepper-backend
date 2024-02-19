3c7b62ac5746167d5e6d3429fce3874cd84ab1da
ryo-093cd <o.ryohei.wk@gmail.com>
2024-02-19 18:49:46 +0900

    テストを作成

--- src/Wav.tsx
+++ src/Wav.tsx
@@ -1,24 +1,23 @@
 export async function submitWav(fname: string): Promise<void> {
+  const formData = new FormData();
+  formData.append('file', fname);
+
+  console.log('request body: ', formData);
+  for (let d of formData.entries()) {
+    console.log(`${d[0]}: ${d[1]}`);
+  }
+
   try {
-    const formData = new FormData();
-    formData.append('file', fname);
-
-    console.log('request body: ', formData);
-    for (let d of formData.entries()) {
-      console.log(`${d[0]}: ${d[1]}`);
-    }
-
-    const response = await fetch('https://examplexxx.com/uploadxxx', {
+    fetch('https://examplexxx.com/uploadxxx', {
       method: 'POST',
       body: formData,
-    });
-
-    if (response.ok) {
-      const responseData = await response.json();
-      console.log('File uploaded successfully:', responseData);
-    } else {
-      throw new Error('Failed to upload file');
-    }
+    })
+      .then((response) => {
+        console.log('File uploaded successfully:', response);
+      })
+      .catch((reason) => {
+        console.error('File upload failed:', reason);
+      });
   } catch (error) {
     console.error('Error uploading file:', error);
   }
--- src/tests/tsx/Wav.test.tsx
+++ src/tests/tsx/Wav.test.tsx
@@ -5,14 +5,26 @@ import App from '../../App';
 import { render, fireEvent } from '@testing-library/react';
 
 describe('send wav file', () => {
-  it('success', async () => {
+  it('should send a text', async () => {
     const { getByLabelText } = render(<App />);
     const file = new File(['wav file content'], 'test.wav', { type: 'audio/wav' });
 
-    (fetch as jest.Mock) = jest.fn().mockResolvedValue({ massage: 'success' });
-    console.log('log;', fetch);
+    (fetch as jest.Mock) = jest.fn().mockResolvedValue('success speech to text');
 
     const inputElement = getByLabelText('uploadfile') as HTMLInputElement;
     fireEvent.change(inputElement, { target: { files: [file] } });
+
+    expect(console.log).toMatchSnapshot();
+  });
+  it('should return error', async () => {
+    const { getByLabelText } = render(<App />);
+    const file = new File(['wav file content'], 'test.wav', { type: 'audio/wav' });
+
+    (fetch as jest.Mock) = jest.fn().mockRejectedValue('error');
+
+    const inputElement = getByLabelText('uploadfile') as HTMLInputElement;
+    fireEvent.change(inputElement, { target: { files: [file] } });
+
+    expect(console.error).toMatchSnapshot();
   });
 });
