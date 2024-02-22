544381c828d6735accc81a097a21f83d5532aa5b
斉藤大明 <saito-hiroaki@saitoudaiminnoMacBook-Air.local>
2024-02-22 19:34:51 +0900

    [feat]レスポンス値の修正

--- src/Wav.tsx
+++ src/Wav.tsx
@@ -1,21 +1,25 @@
-export async function submitWav(file: File): Promise<string> {
+export async function submitWav(file: File): Promise<any> {
   const formData = new FormData();
   formData.append('wavfile', file);
 
-  return new Promise<string>((resolve, reject) => {
+  return new Promise<any>((resolve, reject) => {
     console.log('formData');
     console.log(formData);
     fetch('https://app-gtt-ossp-dev-je-001.azurewebsites.net/', {
       method: 'POST',
       body: formData,
     })
-      .then((response) => {
+      .then(async (response) => {
         if (!response.ok) {
           reject('File upload failed');
         }
-        console.log('File uploaded successfully:', response);
-        const hoge = response.json();
-        resolve(hoge);
+        const data = await response.json();
+        console.log('File uploaded successfully:', data);
+        resolve(`
+          元音声：${data.convertedText}
+          要約結果：${data.summarizedText}
+          カテゴリ：${data.categorizedText}
+        `);
       })
       .catch((reason) => {
         console.error('File upload failed:', reason);
